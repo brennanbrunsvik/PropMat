897fdb7f743018c40ae90d0798fa878d40b255fe
brennanbrunsvik <brennan.brunsvik@gmail.com>
2021-09-07 17:25:00 -0700

    Modified Makefile_wpar to actually compile each file (except the sac executable). Also small modification to saccpt.f.
    Each Makefile was only makeing the file shown in the first block/rule. I had to add the all: clean stuff so that make would try compiling each of the files. There is still one compilation error though, pertaining to sac. It might be with my sac installation, or another problem with saccpt.f.
    
    I changed saccpt.f to allocate n(0:10) instead of n(10), to fix a compilation issue.

--- src/saccpt.f
+++ src/saccpt.f
@@ -1,21 +1,22 @@
 c------------------------------------------------------------
 c     convert output from BYK's programs to SAC format
 c------------------------------------------------------------
-      dimension  y(5000), nn(10), x(1500)
+c bb2021.09.07: nn(10) to nn(0:10) for compile error
+      dimension  y(5000), nn(0:10), x(1500)
       character*32 infil, outfil1, outfil2, outfil3
-	
+
       print *, ' input data file =?'
       read(*,*)infil
       iblank=mblank(infil)
       write(*,*)iblank, mblank(infil)
       outfil1=infil(1:iblank)//'.r'
       outfil2=infil(1:iblank)//'.t'
-      outfil3=infil(1:iblank)//'.z'      
+      outfil3=infil(1:iblank)//'.z'
       ncpt=3
-	
+
       open(1, file=infil)
-      mcpt=0                   
-      nn(0)=0            
+      mcpt=0
+      nn(0)=0
       do 100 icpt=1, ncpt
       read(1,*)
       read(1,*)nn(icpt),dx
@@ -24,15 +25,15 @@ c------------------------------------------------------------
       write(*,*)mcpt
       read(1,*)(y(i+mcpt),i=1,nn(icpt))
   100 continue
-	
+
 
       print *,'# of data for each file ='
       print *,(nn(i),i=1,ncpt)
       n=9999999
       do icpt=1,ncpt
       if(nn(icpt).lt.n)n=nn(icpt)
-      end do                  
-  
+      end do
+
       print *,'updated n=',n
       mcpt=0
       m1=0
@@ -41,7 +42,7 @@ c------------------------------------------------------------
        m1=m1+nn(i-1)
        do j=1, n
         y(j+mcpt)=y(j+m1)
-       end do	
+       end do
       end do
 
 c----------------------------------------------------------
@@ -58,7 +59,7 @@ c---------------------------------------------------------
 
       if(amax1.eq.0.)then
 c--------------------------- use the calculated max.
-      scale=amax/10.           
+      scale=amax/10.
       else
       scale=amax1/10.
       end if
@@ -80,9 +81,9 @@ c   scale y as 0-10, calculate x, and output.
 c==================================================================
 
 
-                               
+
       do 800 icpt=1,ncpt
-                
+
       mcpt=(icpt-1)*n
 
       do i=1,n
@@ -90,26 +91,26 @@ c==================================================================
       end do
 
       if (icpt.eq.1) then
-       call wsac1(outfil1, y, n, 0., dx, neer) 
+       call wsac1(outfil1, y, n, 0., dx, neer)
       end if
 c
       if (icpt.eq.2) then
        do j=1, n
         y(j)=y(j+n)
        end do
-       call wsac1(outfil2, y, n, 0., dx, neer) 
+       call wsac1(outfil2, y, n, 0., dx, neer)
       end if
 c
       if (icpt.eq.3) then
        do j=1, n
         y(j)=y(j+2*n)
        end do
-       call wsac1(outfil3, y, n, 0., dx, neer) 
+       call wsac1(outfil3, y, n, 0., dx, neer)
       end if
 
 
   800 continue
-      
+
       stop
       end
 
@@ -122,4 +123,4 @@ c
       mblank=i-1
       goto 2
  1    continue
- 2    end        
+ 2    end
